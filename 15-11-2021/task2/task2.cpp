70c0dd18301392dec9acf98fcb1a9426d2e06206
MuKaTiR <90700703+MuKaTiR@users.noreply.github.com>
2021-11-15 14:07:30 +0300

    fixes

---
+++ 15-11-2021/task2/task2.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+using namespace std;
+main()
+{
+	int x, max, min, i = 2;
+	cin >> min >> max;
+	while (cin >> x)
+	{
+		i++;
+		if (i % 2)
+		{
+			if (x < min)
+			{
+				min = x;
+			}
+		}
+		else if (x > max)
+		{
+			max = x;
+		}
+	}
+	cout << max + min;
+}
