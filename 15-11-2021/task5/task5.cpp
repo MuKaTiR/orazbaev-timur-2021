e559b39f4647bac8ee492500d73a3c3b98e746da
MuKaTiR <90700703+MuKaTiR@users.noreply.github.com>
2021-11-15 22:22:49 +0300

    task5

---
+++ 15-11-2021/task5/task5.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int numbers1[100001];
+int numbers2[100001];
+
+int main(int argc, char** argv)
+{
+	std::ios::sync_with_stdio(false);
+
+	int n = 0;
+	int m = 0;
+	cin >> n >> m;
+
+	for (int i = 0; i < 100001; ++i)
+	{
+		numbers1[i] = 0;
+		numbers2[i] = 0;
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		int x;
+		cin >> x;
+		numbers1[x] = 1;
+	}
+
+	for (int i = 0; i < m; ++i)
+	{
+		int y;
+		cin >> y;
+		numbers2[y] = 1;
+	}
+
+	for (int i = 0; i < 100001; ++i)
+	{
+		if (numbers1[i] * numbers2[i] == 1)
+		{
+			cout << i << " ";
+		}
+	}
+
+	return EXIT_SUCCESS;
