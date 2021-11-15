a1e2ab4975a925de85af356378a1d2b2fd920304
MuKaTiR <90700703+MuKaTiR@users.noreply.github.com>
2021-11-15 13:43:29 +0300

    15-11

---
+++ kontrolnaya-rabota-15-11/task3/Source.cpp
@@ -0,0 +1,24 @@
+﻿using namespace std;
+#include <iostream>
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int r = 0;
+	int max = 0;
+	int i = 0;
+
+	cin >> a >> b;
+
+	
+	max = a;
+	if (b < a)max = b;
+	for (i = max; i > 0; i--)
+		if (a%i == 0 && b%i == 0)break;
+	
+	cout << (a * b) / i;
+
+	
+
+}
---
+++ kontrolnaya-rabota-15-11/task4/Source.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+ int main()
+{
+	 int a[1000];
+	 int i = 0;
+	 int n = 0;
+	 int x = 0;
+	 int y = 0;
+
+	cin >> n >> a[0];
+
+	x = a[0];
+	y = a[0];
+
+	for (i = 1; i < n; i++)
+	{
+		cin >> a[i];
+		if (a[i] > x)x = a[i];
+		else if (a[i] < y)y = a[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (a[i] == x)a[i] = y;
+		cout << a[i] << ' ';
+	}
+
+	return EXIT_SUCCESS;
+}
