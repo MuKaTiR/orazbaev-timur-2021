62263403ed6c6f8a765b79cb7e31c258e16c978f
MuKaTiR <90700703+MuKaTiR@users.noreply.github.com>
2021-11-15 13:41:47 +0300

    15.11

---
+++ kontrolnaya-rabota-15-11/Source.cpp
@@ -0,0 +1,19 @@
+﻿using namespace std;
+
+#include <iostream>
+
+int main()
+{
+	int m = 0;
+	int n = 0,
+	int k = 0;
+
+	cin >> k;
+	for (int i = 1; i <= k; i++)
+	{
+		cin >> n >> m;
+		cout << 19 * m + (n + 239)*(n + 366) / 2 << endl;
+	}
+	return EXIT_SUCCESS;
+}
+
