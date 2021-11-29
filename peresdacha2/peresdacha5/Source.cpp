692ba40ff24b4d683776c7594c400fe12d7c9b38
MuKaTiR <90700703+MuKaTiR@users.noreply.github.com>
2021-11-29 16:33:58 +0300

    peresdacha2

---
+++ peresdacha2/peresdacha5/Source.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int i = 0;
+	int n = 0;
+	int s = 0;
+	int a[1000];
+
+	cin >> n;
+	for (i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		s = a[0] + a[n - 1] + ((a[1] > a[n - 2]) ? a[1] : a[n - 2]);
+	}
+	for (i = 1; i < n - 1; i++)
+	{
+		if (a[i - 1] + a[i] + a[i + 1] > s)
+        {
+			s = a[i - 1] + a[i] + a[i + 1];
+		    cout << s;
+		}
+	}
+	return 0;
+
+	return EXIT_SUCCESS;
+}
