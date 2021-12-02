83e4432183a1e9a16fdbf416eceb60c5b0516dac
MuKaTiR <90700703+MuKaTiR@users.noreply.github.com>
2021-12-02 11:07:58 +0300

    peresdacha3

---
+++ peresdacha3/peresdacha3/Source.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int  n;
+	int summ = 0;
+	int col = 0;
+	int chislo = 0; 
+	int sra = 0;
+	for (n = 1; n <= 9; n++)
+	{
+		cin >> chislo;
+		summ += chislo;
+	}
+	sra = summ / 9;
+	cout << endl << sra << endl;
+	return EXIT_SUCCESS;
+}
