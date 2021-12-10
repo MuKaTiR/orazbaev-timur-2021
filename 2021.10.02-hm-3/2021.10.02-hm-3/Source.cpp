97d53b3d05297f6c8a3ae5e10a3f5b6c9237020a
MuKaTiR <90700703+MuKaTiR@users.noreply.github.com>
2021-12-10 16:05:48 +0300

    hm3

---
+++ 2021.10.02-hm-3/2021.10.02-hm-3/Source.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int n = 0;
+	cin >> n;
+	int a = 1;
+	while (n > 1)
+	{
+		a *= n;
+		--n;
+	}
+
+	cout << a;
+
+	return EXIT_SUCCESS;
+}
---
+++ 2021.10.02-hm-3/task5/Source.cpp
@@ -0,0 +1,22 @@
+﻿// task5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int n = 0;
+	cin >> n;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		if (n % i == 0)
+		{
+			cout << i << endl;
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
